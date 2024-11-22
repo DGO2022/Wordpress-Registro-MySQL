fc59d1e25faa3d7c5cfcfddb5ba781fd1c7b812b
unknown <davidguerraordonez@gmail.com>
2024-11-21 23:19:34 -0500

    con MySQL

---
+++ src/main/java/com/springboot/WordpressFormularioApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordpressFormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordpressFormularioApplication.class, args);
+	}
+
+}
